9309aa6ee93eb8a795bc1f885ec1b2dcc87e2c36
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-01-11 20:10:03 -0500

    better plots

--- scripts/mprime_tradeoff/plot_data.py
+++ scripts/mprime_tradeoff/plot_data.py
@@ -82,19 +82,19 @@ def plot_comp_delta(m, k, d, deltas):
     plot.axis.kwoptions['legend style'] = r'{font=\tiny}'
     plot.axis.kwoptions['restrict y to domain'] = r'{0:1}'
 
-    palette = p2l.holi(len(deltas)+1)
+    palette = p2l.holi(2*len(deltas)+1)
     plot.add_plot([m,m], [0,1], palette[0], line_width='1pt', opacity='.5', label="\\footnotesize $m'=m$", label_anchor='south')
 
     # First line plot
     delta = deltas[0]
-    color = palette[1]
+    color = palette[2]
     datafile = f'./data/mprime_tradeoff-{m=}_{k=}_{d=}_{delta=}.csv'
     df = pd.read_csv(datafile, sep=',', header=0)
     idx = list(range(0, 2000)) + list(range(2000, 10_000, 500)) + [9_999]
     mprimes = [df['mprime'][i] for i in idx]
     bounds = [df['bound'][i] for i in idx]
 
-    plot.add_plot(mprimes, bounds, color, label=f"\\footnotesize $\\delta={delta}$", line_join='round', label_anchor='-120')
+    plot.add_plot(mprimes, bounds, color, label=f"\\footnotesize $\\delta={delta}$", line_join='round', label_anchor='-135')
 
     min_idx = np.argmin(bounds)
     min_bound = bounds[min_idx]
@@ -104,14 +104,14 @@ def plot_comp_delta(m, k, d, deltas):
 
     # Second line plot
     delta = deltas[1]
-    color = palette[2]
+    color = palette[4]
     datafile = f'./data/mprime_tradeoff-{m=}_{k=}_{d=}_{delta=}.csv'
     df = pd.read_csv(datafile, sep=',', header=0)
     idx = list(range(0, 2000)) + list(range(2000, 10_000, 500)) + [9_999]
     mprimes = [df['mprime'][i] for i in idx]
     bounds = [df['bound'][i] for i in idx]
 
-    plot.add_plot(mprimes, bounds, color, label=f"\\footnotesize $\\delta={delta}$", line_join='round', label_anchor='120')
+    plot.add_plot(mprimes, bounds, color, label=f"\\footnotesize $\\delta={delta}$", line_join='round', label_anchor='135')
 
     min_idx = np.argmin(bounds)
     min_bound = bounds[min_idx]
@@ -132,9 +132,9 @@ def plot_comp_m(ms, k, d, delta):
     plot.axis.kwoptions['legend style'] = r'{font=\tiny}'
     plot.axis.kwoptions['restrict y to domain'] = r'{0:1}'
 
-    palette = p2l.holi(len(ms)+1)
+    palette = p2l.holi(2*len(ms)-1)
 
-    for m, color in zip(ms, palette[1:]):
+    for m, color in zip(ms, palette[::2]):
         plot.add_plot([m,m], [0,1], color=color, line_width='1pt', opacity='.5', label=f"\\footnotesize ${m}$", label_anchor='south')
         datafile = f'./data/mprime_tradeoff-{m=}_{k=}_{d=}_{delta=}.csv'
         df = pd.read_csv(datafile, sep=',', header=0)
@@ -144,7 +144,7 @@ def plot_comp_m(ms, k, d, delta):
 
         mprimes_plot = [mprimes[i] for i in idx]
         bounds_plot = [bounds[i] for i in idx]
-        plot.add_plot(mprimes_plot, bounds_plot, color, label=f"\\footnotesize ${m=}$", line_join='round')
+        plot.add_plot(mprimes_plot, bounds_plot, color, label=f"\\footnotesize ${m=}$", line_join='round', label_anchor='-155')
 
         min_idx = np.argmin(bounds)
         min_bound = bounds[min_idx]
@@ -156,18 +156,25 @@ def plot_comp_m(ms, k, d, delta):
 if __name__ == "__main__":
     os.chdir('./scripts/mprime_tradeoff/')
 
-    ms = [100, 200, 300, 500, 1000]
+    ms = [100, 300, 1000]
+    # ms = [100, 200, 300, 500, 1000]
     deltas = [0.0001, 0.1]
     ks = [0, 10, 30, 50, 100]
     ds = [5, 10, 20, 35, 50]
 
-    filename = f'mprime_tradeoff_comp_delta'
-    doc = p2l.Document(filename, doc_type='standalone')
-    # plot = plot_comp_k(ms[1], ks, ds[1], deltas[2])
-    # plot = plot_comp_d(ms[1], ks[0], ds, deltas[2])
-    plot = plot_comp_delta(ms[1], ks[0], ds[1], deltas)
-    # plot = plot_comp_m(ms, ks[0], ds[1], deltas[2])
+    filename = f'mprime_tradeoff_comp_k'
+    plot = plot_comp_k(200, ks, 10, 0.05)
 
+    filename = f'mprime_tradeoff_comp_d'
+    plot = plot_comp_d(200, 0, ds, 0.05)
+
+    filename = f'mprime_tradeoff_comp_delta'
+    plot = plot_comp_delta(200, 0, 10, deltas)
+
+    filename = f'mprime_tradeoff_comp_m'
+    plot = plot_comp_m(ms, 0, 10, 0.05)
+
+    doc = p2l.Document(filename, doc_type='standalone')
 
     plot.x_min = 5
     plot.x_max = 65_000
@@ -185,5 +192,5 @@ if __name__ == "__main__":
     doc.build()
 
     os.remove(filename + '.log')
-    # os.remove(filename + '.tex')
+    os.remove(filename + '.tex')
     os.remove(filename + '.aux')
