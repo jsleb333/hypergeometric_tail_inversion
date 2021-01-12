3944896f320dc6bad1359d5ab9a6fcaa905df985
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-01-12 17:23:29 -0500

    revert because betaincinv has bug

--- source/binomial_distribution.py
+++ source/binomial_distribution.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.special import betainc, betaincinv
+from scipy.special import betainc
 from scipy.optimize import bisect
 
 
@@ -8,4 +8,6 @@ def binomial_tail(k, m, p):
 
 
 def binomial_tail_inverse(k, m, delta):
-    return 1-betaincinv(m-k, k+1, delta)
+    # Note that one cannot use the regularized incomplete beta function 'betaincinv' of scipy because of numerical instabilities when k is small.
+    func = lambda p: binomial_tail(k, m, p) - delta
+    return bisect(func, 0, 1, xtol=1e-100, rtol=1e-15, maxiter=200)
