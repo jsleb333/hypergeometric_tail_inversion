d50979f1233fff3c617047fc6af3136f589fa6f5
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-01-22 12:52:58 -0500

    quick fix

--- scripts/mprime_tradeoff_relative_deviation/mprime_tradeoff_relative_deviation.py
+++ scripts/mprime_tradeoff_relative_deviation/mprime_tradeoff_relative_deviation.py
@@ -45,39 +45,3 @@ if __name__ == "__main__":
                     print(f'Computing bounds: {mp}/{max_mprime}', end='\r')
                 for mp, bound in enumerate(bounds, start=1):
                     writer.writerow([mp, bound])
-
-    # Computes the optimal value of mprime and saves it
-    best_mprimes = np.zeros((len(ms), len(ks), len(ds), len(deltas)))
-    best_bounds = np.zeros((len(ms), len(ks), len(ds), len(deltas)))
-    bounds_at_mprime_equals_m = np.zeros((len(ms), len(ks), len(ds), len(deltas)))
-
-    for m_idx, k_idx, d_idx, delta_idx in product(range(len(ms)),
-                                                  range(len(ks)),
-                                                  range(len(ds)),
-                                                  range(len(deltas))
-                                                  ):
-        m, k, d, delta = ms[m_idx], ks[k_idx], ds[d_idx], deltas[delta_idx]
-
-        filepath = path + f'mprime_tradeoff-{m=}_{k=}_{d=}_{delta=}.csv'
-        df = pd.read_csv(filepath, sep=',', header=0)
-
-        mprimes, bounds = df['mprime'], df['bound']
-        min_idx = np.argmin(bounds)
-        best_mprimes[m_idx, k_idx, d_idx, delta_idx] = mprimes[min_idx]
-        best_bounds[m_idx, k_idx, d_idx, delta_idx] = bounds[min_idx]
-        bounds_at_mprime_equals_m[m_idx, k_idx, d_idx, delta_idx] = bounds[m-1]
-
-    data = xr.Dataset(
-        data_vars={
-            'bound': (['m', 'k', 'd', 'delta'], best_bounds),
-            'bound_at_mprim=m': (['m', 'k', 'd', 'delta'], bounds_at_mprime_equals_m),
-            'mprime': (['m', 'k', 'd', 'delta'], best_mprimes)
-        },
-        coords={
-            'm': ms,
-            'k': ks,
-            'd': ds,
-            'delta': deltas
-        }
-    )
-    data.to_netcdf(path + 'optimal_bound_relative_deviation.nc')
