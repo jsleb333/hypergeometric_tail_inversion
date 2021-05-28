437564b25b97a6f7ebc105853041fab5c8ee6ba5
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-05-28 10:39:23 -0400

    make code cleaner

--- scripts/bounds_comparison/bounds_comparison_m_plot.py
+++ scripts/bounds_comparison/bounds_comparison_m_plot.py
@@ -1,109 +1,110 @@
 import numpy as np
 import os, sys
 sys.path.append(os.getcwd())
-import pandas as pd
+os.chdir('./scripts/bounds_comparison/')
 
 import python2latex as p2l
-from graal_utils import Timer, timed
+from graal_utils import Timer
 
-from source import optimize_mprime, optimize_catoni
 from source import hypinv_upperbound, vapnik_pessismistic_bound, vapnik_relative_deviation_bound, catoni_4_6
 from source.utils import sauer_shelah
 
 
-risk, d, delta = 0.05, 50, 0.05
-print(f'{risk=}, {d=}, {delta=}')
-ms = np.array(
-    list(range(d, 500, 10))
-    + list(range(500, 1000, 50))
-    + [int(m) for m in np.logspace(10, 20, num=40, base=2)]
-)
-ks = np.array([int(risk*m) for m in ms])
+def plot_comp_m(risk, d, delta=0.05):
+    ms = np.array(
+        list(range(d, 1000, 10))
+        + list(range(1000, 10_000, 100))
+        + [int(m) for m in np.logspace(13.5, 20, num=10, base=2)]
+    )
+    ks = np.array([int(risk*m) for m in ms])
 
-os.chdir('./scripts/bounds_comparison/')
-# path = './data/'
-# os.makedirs(path, exist_ok=True)
-# filename = f'best_mprimes_{risk=}_{d=}_{delta=}.csv'
-# df = pd.read_csv(path + filename, sep=',', header=0)
+    plot = p2l.Plot(plot_name=f'bounds_comp_m_{risk=}_{d=}_{delta=}',
+                    plot_path='figures',
+                    as_float_env=False,
+                    width='7.45cm',
+                    height='6cm',
+                    lines='1pt',
+                    xmode='log',
+                    ymode='log',
+                    palette=reversed(p2l.holi(4))
+                    )
+    plot.axis.kwoptions['legend style'] = r'{font=\scriptsize}'
+    plot.axis.kwoptions['y label style'] = r'{yshift=-.2cm}'
+    plot.axis.kwoptions['legend cell align'] = '{left}'
 
+    plot.x_min = d
+    plot.x_max = 1e6
+    plot.y_min = 0
+    plot.y_max = 1.1
+    plot.x_label = "Sample size $m$"
+    plot.y_label = 'Upper bound on $R_\mathcal{D}(h) - R_S(h)$'
+    plot.legend_position = 'south west'
+    # plot.legend_position = 'north east'
 
-plot = p2l.Plot(plot_name=f'bounds_comp_m_{d=}_{delta=}',
-                plot_path='figures',
-                as_float_env=False,
-                width='7.45cm',
-                height='6cm',
-                lines='1pt',
-                xmode='log',
-                ymode='log',
-                palette=reversed(p2l.holi(4))
-                )
-plot.axis.kwoptions['legend style'] = r'{font=\scriptsize}'
-plot.axis.kwoptions['y label style'] = r'{yshift=-.2cm}'
-plot.axis.kwoptions['legend cell align'] = '{left}'
+    # # Lugosi
+    # with Timer('LC'):
+    #     plot.add_plot(ms, lugosi_chaining(ks, ms, d, delta)-risk, legend='Lugosi')
 
-plot.x_min = d
-plot.x_max = 1e6
-plot.y_min = 0
-plot.y_max = 1.1
-plot.x_label = "Sample size $m$"
-plot.y_label = 'Upper bound on $R_\mathcal{D}(h) - R_S(h)$'
-plot.legend_position = 'south west'
-# plot.legend_position = 'north east'
+    # VRD
+    with Timer('VRD'):
+        bound_values = vapnik_relative_deviation_bound(ks, ms, sauer_shelah(d), delta)
+        print(ms[np.argmin((bound_values - .5)**2)])
+        plot.add_plot(ms, bound_values - risk, legend='VRD')
 
-# # Lugosi
-# print('Lugosi')
-# plot.add_plot(ms,
-#               lugosi_chaining(ks, ms, d, delta)-risk,
-#             #   'dotted',
-#               legend='Lugosi')
+    # VP
+    with Timer('VP'):
+        bound_values = vapnik_pessismistic_bound(ks, ms, sauer_shelah(d), delta)
+        print(ms[np.argmin((bound_values - .5)**2)])
+        plot.add_plot(ms, bound_values - risk, legend='VP')
 
-# VRD
-with Timer('VRD'):
-    plot.add_plot(ms,
-                vapnik_relative_deviation_bound(ks, ms, sauer_shelah(d), delta)-risk,
-                legend='VRD')
+    # Catoni
+    with Timer('C4.6'):
+        bound_values = np.array([catoni_4_6(k, m, d, delta, mprime=None) for k, m in zip(ks, ms)])
+        print(ms[np.argmin((bound_values - .5)**2)])
+        plot.add_plot(ms, bound_values - risk, legend='C4.6')
 
-# VP
-with Timer('VP'):
-    plot.add_plot(ms,
-                vapnik_pessismistic_bound(ks, ms, sauer_shelah(d), delta)-risk,
-                legend='VP')
+    # HTI
+    with Timer('HTI'):
+        def mprime(k, m):
+            # return sum(coef*param for coef, param in zip(np.polyfit([d, 1e6], [3.5*m, 9*m], 1), [m, 1]))
+            best_mp = int(3.25*m)
+            best_bound = 1
+            for ratio in np.arange(3.25, 13, step=.25):
+                mp = int(m*ratio)
+                bound = hypinv_upperbound(k, m, sauer_shelah(d), delta, mp)
+                if bound < best_bound:
+                    best_bound = bound
+                    best_mp = mp
+                elif bound > best_bound:
+                    break
+            # print(m, best_mp, best_mp/m)
+            return best_mp
 
-# Catoni
-with Timer('C4.6'):
-    plot.add_plot(ms,
-                [catoni_4_6(k, m, d, delta, mprime=optimize_catoni(k, m, d, delta)[1])-risk for k, m in zip(ks, ms)],
-                legend='C4.6')
-
-# HTI
-with Timer('HTI'):
-    def mprime(k, m):
-        # return sum(coef*param for coef, param in zip(np.polyfit([d, 1e6], [3.5*m, 9*m], 1), [m, 1]))
-        best_mp = int(3.25*m)
-        best_bound = 1
-        for mp in np.linspace(3.25*m, 10*m, num=(10-3)*4):
-            mp = int(mp)
-            bound = hypinv_upperbound(k, m, sauer_shelah(d), delta, mp)
-            if bound < best_bound:
-                best_bound = bound
-                best_mp = mp
-            # elif bound > best_bound:
-            #     break
-        # print(m, best_mp)
-        return best_mp
-
-    plot.add_plot(ms,
-                  [hypinv_upperbound(k, m, sauer_shelah(d), delta, mprime=mprime(k, m))-risk for k, m in zip(ks, ms)],
-                  #   df['HTI-bound']-risk,
-                  legend='HTI')
+        bound_values = np.array([hypinv_upperbound(k, m, sauer_shelah(d), delta, mprime=mprime(k, m)) for k, m in zip(ks, ms)])
+        print(ms[np.argmin((bound_values - .5)**2)])
+        plot.add_plot(ms, bound_values - risk, legend='HTI')
 
 
-plot.add_plot(ms, np.sqrt(d/ms), color='gray', line_width='.5pt', legend=r'\scalebox{.8}{\normalsize $\sqrt{d/m}$}')
+    if risk > 0:
+        plot.add_plot(ms, np.sqrt(d/ms), color='gray', line_width='.5pt', legend=r'\scalebox{.8}{\normalsize $\sqrt{d/m}$}')
+    else:
+        plot.add_plot(ms, d/ms, color='gray', line_width='.5pt', legend='$d/m$')
 
-filename = 'bounds_comparison_m'
-doc = p2l.Document(filename, doc_type='standalone')
-doc.add_package('mathalfa', cal='dutchcal', scr='boondox')
-doc.add_package('times')
-doc += plot
-print('Building...')
-doc.build(delete_files='all')
+    filename = f'bounds_comparison_m_{risk=}_{d=}'
+    doc = p2l.Document(filename, doc_type='standalone')
+    doc.add_package('mathalfa', cal='dutchcal', scr='boondox')
+    doc.add_package('times')
+    doc += plot
+    print('Building...')
+    doc.build(delete_files='all', show_pdf=False)
+    print('Building completed.')
+
+
+if __name__ == '__main__':
+    risk, d, delta = 0.05, 50, 0.05
+    print(f'{risk=}, {d=}, {delta=}')
+
+    # for risk in [0, 0.1, 0.2, 0.3]:
+    #     plot_comp_m(risk, d, delta)
+
+    plot_comp_m(risk, 10, delta)
--- scripts/bounds_comparison/bounds_comparison_risk.py
+++ scripts/bounds_comparison/bounds_comparison_risk.py
@@ -1,111 +1,127 @@
 import numpy as np
 import os, sys
 sys.path.append(os.getcwd())
+os.chdir('./scripts/bounds_comparison/')
 
 import python2latex as p2l
-from graal_utils import Timer
+from graal_utils import Timer, timed
 
-from source import optimize_mprime
+from source import optimize_mprime, optimize_catoni
 from source import hypinv_upperbound, hypinv_reldev_upperbound, vapnik_pessismistic_bound, vapnik_relative_deviation_bound, sample_compression_bound, catoni_4_6
 from source.utils import sauer_shelah
 
 
-m, d, delta = 2000, 50, 0.05
-# mp = optimize_mprime(0, m, sauer_shelah(d), delta, max_mprime=10*m, min_mprime=3*m, early_stopping=1000)
-# print(mp)
-# mp = 678 # Optimization returns this number when run with m=200, d=15 and delta=0.05
-# mp = 1693 # Optimization returns this number when run with m=500, d=20 and delta=0.05
-# mp = 4071 # Optimization returns this number when run with m=1000, d=10 and delta=0.05
-# mp = 3611 # Optimization returns this number when run with m=1000, d=20 and delta=0.05
-# mp = 5937 # Optimization returns this number when run with m=1500, d=15 and delta=0.05
-mp = 6970 # Optimization returns this number when run with m=2000, d=50 and delta=0.05
-# mp = m
+# Saved values of optimized m' for parameters (m, d, delta) using 'optimize_mprime(0, m, sauer_shelah(d), delta, max_mprime=13*m, min_mprime=3*m, early_stopping=1000)'
+mp_dict = {
+    (100, 50, 0.05): 388,
+    (200, 15, 0.05): 678,
+    (500, 20, 0.05): 1673,
+    (500, 50, 0.05): 1578,
+    (1000, 10, 0.05): 4071,
+    (1000, 20, 0.05): 3611,
+    (1500, 15, 0.05): 5937,
+    (2000, 10, 0.05): 8762,
+    (2000, 50, 0.05): 6970,
+    (20_000, 50, 0.05): 91594,
+}
+
+def plot_risk_comp(m, d, delta=0.05, show_non_opti=False):
+    catoni_mp = optimize_catoni(0, m, d, delta)[1]
+    print(catoni_mp/m)
+
+    if (m, d, delta) in mp_dict:
+        mp = mp_dict[(m, d, delta)]
+    else:
+        mp = optimize_mprime(0, m, sauer_shelah(d), delta, max_mprime=13*m, min_mprime=3*m, early_stopping=1000)
+        print(f'Optimal mprime for params ({m=}, {d=}, {delta=}): {mp=}')
+
+    plot = p2l.Plot(plot_name=f'bounds_comp_{m=}_{d=}_{delta=}',
+                    plot_path='figures',
+                    as_float_env=False,
+                    width='7.45cm',
+                    height='6cm',
+                    lines='1pt',)
+    plot.axis.kwoptions['legend style'] = r'{font=\scriptsize}'
+    plot.axis.kwoptions['y label style'] = r'{yshift=-.2cm}'
+    plot.axis.kwoptions['legend cell align'] = '{left}'
+
+    palette = p2l.holi(4)
+
+    bounds = [
+        (
+            "HTI\\textsubscript{opti}",
+            lambda k: hypinv_upperbound(k, m, sauer_shelah(d), delta, mprime=mp),
+            "",
+            palette[0]
+        ),
+        (
+            "HTI$_{m'=m}$",
+            lambda k: hypinv_upperbound(k, m, sauer_shelah(d), delta, mprime=m),
+            "dashed",
+            palette[0]
+        ),
+        (
+            "C4.6", # Catoni (2004)'s Theorem 4.6
+            lambda k: catoni_4_6(k, m, d, delta, mprime=catoni_mp),
+            '',
+            palette[1]
+        ),
+        (
+            "VP",
+            lambda k: vapnik_pessismistic_bound(k, m, sauer_shelah(d), delta),
+            "",
+            palette[2]
+        ),
+        (
+            "VRD",
+            lambda k: vapnik_relative_deviation_bound(k, m, sauer_shelah(d), delta),
+            "",
+            palette[3]
+        ),
+    ]
+    if not show_non_opti:
+        del bounds[1]
+    bounds.reverse()
+
+    ks = np.array([int(k) for k in np.linspace(0, m, num=100)])
+    for name, bound, style, color in bounds:
+        with Timer(name):
+            bs = [bound(k) for k in ks]
+            plot.add_plot(ks/m, bs, style, color=color, legend=name)
+            print(name, bs[0])
+
+    plot.add_plot([0,1], [0,1], line_width='.5pt', color='gray!50')
+
+    plot.x_min = 0
+    plot.x_max = 1.02
+    plot.y_min = 0
+    plot.y_max = 1.02
+    plot.y_ticks = np.linspace(0, 1.25, 6)
+
+    plot.x_label = "Empirical risk $R_S(h)$"
+    plot.y_label = r'Upper bound on the true risk $R_\mathcal{D}(h)$'
+    plot.legend_position = 'south east'
 
 
-plot = p2l.Plot(plot_name=f'bounds_comp_{m=}_{d=}_{delta=}',
-                plot_path='figures',
-                as_float_env=False,
-                width='7.45cm',
-                height='6cm',
-                lines='1pt',)
-plot.axis.kwoptions['legend style'] = r'{font=\scriptsize}'
-plot.axis.kwoptions['y label style'] = r'{yshift=-.3cm}'
-plot.axis.kwoptions['legend cell align'] = '{left}'
 
-palette = p2l.holi(4)
+    filename = f'bounds_comparison_risk_{m=}_{d=}'
+    if show_non_opti:
+        filename += '_with_non_opti'
 
-bounds = [
-    # (
-    #     "SC",
-    #     # "Sample compression",
-    #     lambda k: sample_compression_bound(k, m, d, delta),
-    #     # "dotted",
-    #     '',
-    #     palette[-1]
-    # ),
-    (
-        "HTI\\textsubscript{opti}",
-        lambda k: hypinv_upperbound(k, m, sauer_shelah(d), delta, mprime=mp),
-        "",
-        palette[0]
-    ),
-    (
-        "HTI$_{m'=m}$",
-        lambda k: hypinv_upperbound(k, m, sauer_shelah(d), delta, mprime=m),
-        "dashed",
-        palette[0]
-    ),
-    (
-        "C4.6", # Catoni (2004)'s Theorem 4.6
-        lambda k: catoni_4_6(k, m, d, delta),
-        # "dotted",
-        '',
-        palette[1]
-    ),
-    (
-        "VP",
-        lambda k: vapnik_pessismistic_bound(k, m, sauer_shelah(d), delta),
-        # "dash dot"
-        "",
-        palette[2]
-    ),
-    (
-        "VRD",
-        lambda k: vapnik_relative_deviation_bound(k, m, sauer_shelah(d), delta),
-        # "dash dot"
-        "",
-        palette[3]
-    ),
-]
-bounds.reverse()
+    doc = p2l.Document(filename, doc_type='standalone')
+    doc.add_package('times')
+    doc.add_package('mathalfa', cal='dutchcal', scr='boondox')
+    doc += plot
 
-ks = np.arange(0, m, 5)
-for name, bound, style, color in bounds:
-    with Timer(name):
-        bs = [bound(k) for k in ks]
-        plot.add_plot(ks/m, bs, style, color=color, legend=name)
-        print(name, bs[0])
-
-plot.add_plot([0,1], [0,1], line_width='.5pt', color='gray!50')
-
-plot.x_min = 0
-plot.x_max = 1.02
-plot.y_min = 0
-plot.y_max = 1.02
-plot.y_ticks = np.linspace(0, 1.25, 6)
-
-plot.x_label = "Empirical risk $R_S(h)$"
-plot.y_label = 'Upper bound on the true risk'
-plot.legend_position = 'south east'
+    print('Building...')
+    doc.build(delete_files='all', show_pdf=False)
+    print('Building completed.')
 
 
-os.chdir('./scripts/bounds_comparison/')
+if __name__ == '__main__':
+    d, delta = 10, 0.05
+    # for m in [100, 500, 2000, 20_000]:
+    #     with Timer(f'{m=}'):
+    #         plot_risk_comp(m, d, delta)
 
-filename = 'bounds_comparison_risk'
-
-doc = p2l.Document(filename, doc_type='standalone')
-doc.add_package('times')
-doc += plot
-
-print('Building...')
-doc.build(delete_files='all')
+    plot_risk_comp(2000, d, delta, show_non_opti=True)
