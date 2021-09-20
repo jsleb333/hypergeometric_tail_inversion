a578fbb682b810422529d369eab533186c91efbc
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-09-20 13:12:41 -0400

    Revert "plot as function of d"
    This reverts commit dd531bd52c14179a8ab42a47496e1a37701c5d42.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from version import version
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hypergeo",
+    version=version,
+    author="Jean-Samuel Leboeuf",
+    author_email="jean-samuel.leboeuf.1@ulaval.ca",
+    description="An implementation of the hypergeometric tail pseudo-inverse used in the paper Improving generalization bounds for VC classes using the hypergeometric tail inversion.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsleb333/hypergeo",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
