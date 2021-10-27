43b53b05fbb6e08de7fdfc982c69d6813e880c26
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2021-10-27 10:29:14 -0400

    update version and requirements

--- hypergeo/__init__.py
+++ hypergeo/__init__.py
@@ -2,3 +2,5 @@ from hypergeo.hypergeometric_distribution import *
 from hypergeo.binomial_distribution import *
 from hypergeo.generalization_bounds import *
 from hypergeo import utils
+
+from version import __version__
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
-from version import version
+from version import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="hypergeo",
-    version=version,
+    version=__version__,
     author="Jean-Samuel Leboeuf",
     author_email="jean-samuel.leboeuf.1@ulaval.ca",
     description="An implementation of the hypergeometric tail pseudo-inverse used in the paper Improving generalization bounds for VC classes using the hypergeometric tail inversion.",
@@ -17,7 +17,7 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'scipy',
-        'python2latex==0.4.0',
+        'python2latex==0.4.1',
         'colorama',
         'graal_utils @ git+https://github.com/GRAAL-Research/graal-utils.git',
         'pandas',
@@ -29,4 +29,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    data_files=[('', ['version.py'])]
 )
