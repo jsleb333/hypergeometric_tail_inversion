d1d1fb048a8d83319989c0b7bc67f97af6cb4ab2
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2022-02-04 13:40:11 -0500

    fix tests

--- tests/test_generalization_bounds.py
+++ tests/test_generalization_bounds.py
@@ -1,22 +1,6 @@
 from hypergeo.generalization_bounds import *
 
 
-def test_optimize_mprime_max_mode():
-    k, m, delta, d = 5, 50, 0.05, 5
-    growth_function = lambda M: (np.e*M/d)**d
-    mprime = optimize_mprime(
-        k=k,
-        m=m,
-        growth_function=growth_function,
-        delta=delta,
-        max_mprime=5*m,
-        bound=hypinv_lowerbound,
-        optimization_mode='max',
-    )
-    print(mprime)
-    raise
-
-
 def test_hypinv_upperbound():
     k, m = 5, 50
     d = 5
@@ -29,12 +13,12 @@ def test_hypinv_upperbound():
 
 
 def test_hypinv_lowerbound():
-    k, m = 5, 50
-    d = 5
+    k, m = 100, 500
+    d = 10
     growth_function = lambda M: (np.e*M/d)**d
 
-    best_bound = hypinv_lowerbound(k, m, growth_function, max_mprime=5*m)
+    best_bound = hypinv_lowerbound(k, m, growth_function, mprime=4*m)
 
     assert best_bound <= 1
     assert best_bound >= 0
-    assert hypinv_lowerbound(k, m, growth_function, mprime=m) < best_bound
+    assert hypinv_lowerbound(k, m, growth_function, mprime=3*m) < best_bound
